817c995e42cf04103e22f502505a0085c1497d5d
liumapp <liumapp.com@gmail.com>
2017-10-10 14:48:46 +0800

    分离出helper

--- src/main/java/com/liumapp/KeyGenerator/Main.java
+++ src/main/java/com/liumapp/KeyGenerator/Main.java
@@ -2,6 +2,7 @@ package com.liumapp.KeyGenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 /**
  * Created by liumapp on 9/28/17.
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * home-page:http://www.liumapp.com
  */
 @SpringBootApplication(scanBasePackages = {"com.liumapp"})
+@Import(com.liumapp.KeyGeneratorHelper.Main.class)
 public class Main {
 
     public static void main(String[] args) {
